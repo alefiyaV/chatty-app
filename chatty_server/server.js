7f73550ccff9593a603d5911efa034b7ff3392c9
Alefiya <alefiyavahanvaty@gmail.com>
2018-10-05 03:30:09 +0000

    added on close broadcast so that number of clients updates when they disconnect too

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -22,8 +22,6 @@ wss.broadcast = function broadcast(data) {
    }
  })
 }
-
-
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
@@ -80,128 +78,13 @@ wss.on('connection', function connection(ws) {
 
     //console.log('this is the passed data: %s', data);
   })
-  ws.on('close', () => console.log('Client disconnected'));
+
+ws.on('close', () => {
+  console.log('Client disconnected')
+  let disconnectedClients = ({
+    type: "disconnectedClients",
+    content: wss.clients.size
+  })
+  wss.broadcast(JSON.stringify(disconnectedClients));
 });
-
-// clients = [];
-
-// ws.broadcast = function broadcast(data) {
-//  wss.clients.forEach(function each(client) {
-//    if (client.readyState === SocketServer.OPEN) {
-//     client.send(JSON.stringify(data));
-//     console.log('sent: ', data);
-//    }
-//  })
-// })
-
-//   
-// }
-
-// setInterval(broadcast, 3000);
-
-// // server.js
-
-// const express = require('express');
-// const socket = require('ws');
-// const SocketServer = socket.Server;
-// const uuidv1 = require('uuid/v1');
-
-// // Set the port to 3001
-// const PORT = 3001;
-
-// // Create a new express server
-// const server = express()
-//    // Make the express server serve static assets (html, javascript, css) from the /public folder
-//   .use(express.static('public'))
-//   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
-
-// // Create the WebSockets server
-// const wss = new SocketServer({ server });
-
-// wss.broadcast = function broadcast(data) {
-//   //console.log("these are the clients", wss.clients)
-//   //console.log("are these clients?", wss.clients.length)
-//  wss.clients.forEach(function each(client) {
-   
-//    if (client.readyState === socket.OPEN) {
-//     client.send(data);
-//     //console.log('sent: ', data);
-//    }
-//  })
-// }
-
-
-// // Set up a callback that will run when a client connects to the server
-// // When a client connects they are assigned a socket, represented by
-// // the ws parameter in the callback.
-
-
-// wss.on('connection', function connection(ws) {
-
-//   let howManyClients = {
-//       content: wss.clients.size
-//     }
-//   }
-//   // wss.clients.size
-//   // let connectedClients ={
-//   //   type
-//   //   number
-//   // }
-//   // wss.broadcast(JSON.stringify(connectedClients))
-//   console.log("ws connection", wss.clients.size)
-
-//   ws.on('message', function incoming(data) {
-//     //console.log("old data", data);
-//     let newData = JSON.parse(data);
-
-//     console.log("this is the very newest data", newData)
-
-//     switch(newData.type) {
-//       case "postMessage":
-//       newMessage = {
-//         type: "incomingMessage",
-//         id: uuidv1(),
-//         username: newData.username,
-//         content: newData.content
-//       };
-//       break;
-
-//       case "postNotification":
-//         newMessage = {
-//           type: "incomingNotification",
-//           content: newData.content
-          
-//       };
-//       break;
-
-//       //case "clientConnected":
-//       //howManyClients = {
-//       //  content: wss.clients.size
-//       }
-//       break;
-
-//     }
-    
-//     wss.broadcast(JSON.stringify(newMessage))
-
-//   })
-//   ws.on('close', () => 
-//   console.log("ws connection", wss.clients.size)
-//   );
-// });
-
-// // clients = [];
-
-// // ws.broadcast = function broadcast(data) {
-// //  wss.clients.forEach(function each(client) {
-// //    if (client.readyState === SocketServer.OPEN) {
-// //     client.send(JSON.stringify(data));
-// //     console.log('sent: ', data);
-// //    }
-// //  })
-// // })
-
-// //   
-// // }
-
-// // setInterval(broadcast, 3000);
+});
